6f734099b9b3dee46bd686b54dd6935cf9cba1a3
Divyamverma626 <74546867+Divyamverma626@users.noreply.github.com>
2020-11-16 18:50:33 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+let strip = neopixel.create(DigitalPin.P0, 1, NeoPixelMode.RGB_RGB)
+basic.forever(function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(500)
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.pause(500)
+    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+})
